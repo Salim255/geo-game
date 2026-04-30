b3c64762d4fb385df9a495e8bfa74225a90402b0
Salim <doshka23@gmail.com>
2026-04-30 18:48:28 +0200

    refined: header style

--- src/app/on-boarding/components/header/header.component.ts
+++ src/app/on-boarding/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-header",
@@ -6,4 +7,10 @@ import { Component } from "@angular/core";
   styleUrl: "./header.component.scss",
   standalone: false
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(private router: Router){}
+
+  home(){
+    this.router.navigate(["/home"])
+  }
+}
