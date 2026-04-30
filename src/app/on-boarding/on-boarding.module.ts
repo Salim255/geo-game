115930a43129339877c75c9b26e845e0619eccdd
Salim <doshka23@gmail.com>
2026-04-30 17:48:39 +0200

    built:

--- src/app/on-boarding/on-boarding.module.ts
+++ src/app/on-boarding/on-boarding.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from "@angular/core";
 import { ObBoardingRoutingModule } from "./on-boarding-routing.module";
 import { CommonModule } from "@angular/common";
+import { HeaderComponent } from "./components/header/header.component";
 
 @NgModule({
+  declarations: [HeaderComponent],
   imports: [
     CommonModule,
     ObBoardingRoutingModule,
