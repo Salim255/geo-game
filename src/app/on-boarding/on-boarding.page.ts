97bd009b53980f9bdf55892bf83e5b37202f1962
Salim <doshka23@gmail.com>
2026-04-30 17:31:19 +0200

    added: OnBoardingPage

---
+++ src/app/on-boarding/on-boarding.page.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-on-boarding",
+  templateUrl: "./on-boarding.page.html",
+  styleUrl: "./on-boarding.page.scss",
+})
+export class OnBoardingPage {}
