6e8a7098bcf0dcdff9dbfb5a8842a7518fd1bb23
Salim <doshka23@gmail.com>
2026-05-04 22:52:55 +0200

    added: StartScreenModule

---
+++ src/app/features/start-screen/start-screen.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { StartScreenRoutingModule } from "./start-screen-routing.module";
+import { StartScreenPage } from "./start-screen.page";
+
+@NgModule({
+  imports: [StartScreenRoutingModule],
+  declarations: [StartScreenPage]
+})
+
+export class StartScreenModule {}
