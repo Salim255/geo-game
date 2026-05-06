93a1b7c344335b05eb0a0979b4d7585375b37f12
Salim <doshka23@gmail.com>
2026-05-06 17:31:35 +0200

    built: ActionService

---
+++ src/app/features/game-screen/services/action.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { ActionScreenComponent } from "../components/action-screen/action-screen.component";
+import { MatDialog } from "@angular/material/dialog";
+
+@Injectable({providedIn: "root"})
+export class ActionService {
+
+  constructor(private modalCtr: MatDialog ){}
+
+  openActionModal() {
+      this.modalCtr.open(ActionScreenComponent , {
+        disableClose: false,
+        panelClass: 'dialog-wide',
+        maxWidth: "96vw",
+        maxHeight: "96vh",
+        data: {
+          title: 'Action Screen'
+        }
+      });
+    }
+
+}
--- src/app/shared/kits/modals/bg-modal/bg-modal.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-bg-modal",
-  templateUrl: "./bg-modal.component.html",
-  styleUrl: "./bg-modal.component.scss",
-  standalone: false
-})
-export class BgModalComponent {}
