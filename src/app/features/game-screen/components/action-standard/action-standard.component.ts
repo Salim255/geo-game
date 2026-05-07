1ba3ac714efbfd2d1fc591a86c57f8702119421f
Salim <doshka23@gmail.com>
2026-05-07 10:03:51 +0200

    added: ActionStandardComponent

---
+++ src/app/features/game-screen/components/action-standard/action-standard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acton-standard',
+  templateUrl: './action-standard.component.html',
+  styleUrl: "./action-standard.component.scss",
+  standalone: false
+})
+
+export class ActionStandardComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
+
