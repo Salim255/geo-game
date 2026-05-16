37a9321286649dadee230cbce50430470b6569a8
Salim <doshka23@gmail.com>
2026-05-16 10:35:32 +0200

    adde: corrected bug

--- src/app/features/game-screen/components/map/map.component.ts
+++ src/app/features/game-screen/components/map/map.component.ts
@@ -115,18 +115,18 @@ export class MapComponent implements OnInit, OnDestroy {
     const userIcon = this.createUserIcon();
 
     // 🧪 DEV MODE
-       this.gps.startFakeTracking(
+    /*    this.gps.startFakeTracking(
       50.63061531074475,
       3.010675532644488,
       (pos) => this.handlePosition(pos, userIcon)
-    );
+    ); */
 
     // 📍 PROD MODE
 
-  /*  this.gps.startTracking((pos) =>
+    this.gps.startTracking((pos) =>
       this.handlePosition(pos, userIcon)
     );
- */
+
   }
 
   private handlePosition(pos: GeolocationPosition, icon: L.DivIcon) {
